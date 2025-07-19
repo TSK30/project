7736ba2959d4b077ac5123340733ccf41b425c75
karthikts@byclaritytech.com <karthikts@byclaritytech.com>
2025-07-19 10:06:30 +0530

    update campaign

--- src/screens/CampaignSetup/CampaignSetup.jsx
+++ src/screens/CampaignSetup/CampaignSetup.jsx
@@ -114,7 +114,7 @@ export const CampaignSetup = () => {
                       >
                         <IconComponent
                           className={`w-5 h-5 mr-3 ${
-                            item.active ? "text-purple-600" : "text-gray-600"
+                            item.active ? "text-[DARK]" : "text-gray-600"
                           }`}
                         />
                         <span
